16448ff98fe40b6686d261e6482a02115b5b4b97
grimi_tiny <111630877+grimi-tiny@users.noreply.github.com>
2023-07-27 21:19:14 +0700

    remake table with description tarifs

--- src/components/view-start/ViewStartTraiffLinkBlock.vue
+++ src/components/view-start/ViewStartTraiffLinkBlock.vue
@@ -88,61 +88,86 @@
             </div>
         </div>
         <div class="tariff-description">
-            <div class="question-tarif">КАКОЙ ТАРИФ ПОДОЙДЕТ ИМЕННО ВАМ?</div>
-            <div class="type-description">
-                <div class="base-description">
-                    <div class="base-name">БАЗОВЫЙ</div>
-                    <div class="base-name-description-1">
+            КАКОЙ ТАРИФ ПОДОЙДЕТ ИМЕННО ВАМ?
+        </div>
+        <div class="tariff-types-wrapper">
+            <div class="tariffs-name">
+                <h1 class="base">
+                    БАЗОВЫЙ
+                </h1>
+            </div>
+            <div class="tariffs-name">
+                <h1 class="advanced">
+                    ПРОДВИНУТЫЙ
+                </h1>
+            </div>
+            <div class="tariffs-name">
+                <h1 class="professional">
+                    ПРОФЕССИОНАЛЬНЫЙ
+                </h1>
+            </div>
+            <div class="description-white-line">
+                <div class="description">
+                    <h1>
                         Малый бизнес,
                         не имеющий больших
                         средств на более
                         дорогую подписку
-                    </div>
-                    <div class="base-name-description-2">
-                        Бизнес не имеет продаж,
-                        поэтому нуждается в самой
-                        базовой аналитике
-                    </div>
+                    </h1>
                 </div>
-                <div class="advanced-description">
-                    <div class="advanced-name">ПРОДВИНУТЫЙ</div>
-                    <div class="advanced-name-description-1">
-                        Малый и средний бизнес,
+            </div>
+            <div class="description-white-line">
+                <div class="description">
+                    <h1>Малый и средний бизнес,
                         нуждающийся в полном
                         функционале
                         рекомендательной
                         системы для принятия
                         решений
-                    </div>
-                    <div class="advanced-name-description-2">
+                    </h1>
+                </div>
+            </div>
+            <div class="description">
+                <h1>
+                    Малый и средний бизнес,
+                    активно масштабирующийся
+                    или развивающий разные
+                    направления; менеджеры
+                    маркетплейсов, работающие
+                    с несколькими клиентами
+                    одновременно
+                </h1>
+            </div>
+            <div class="description-white-line">
+                <div class="description">
+                    <h1 class="description-line">
+                        Бизнес не имеет продаж,
+                        поэтому нуждается в самой
+                        базовой аналитике
+                    </h1>
+                </div>
+            </div>
+            <div class="description-white-line">
+                <div class="description">
+                    <h1 class="description-line">
                         Бизнес стабилен, но есть
                         потребность
                         в масштабировании
                         и увеличении продаж;
                         есть запрос на
                         автоматизацию бизнеса
-                    </div>
+                    </h1>
                 </div>
-                <div class="professional-description">
-                    <div class="professional-name">ПРОФЕССИОНАЛЬНЫЙ</div>
-                    <div class="professional-name-description-1">
-                        Малый и средний бизнес,
-                        активно масштабирующийся
-                        или развивающий разные
-                        направления; менеджеры
-                        маркетплейсов, работающие
-                        с несколькими клиентами
-                        одновременно
-                    </div>
-                    <div class="professional-name-description-2">
-                        Иметь весь возможный
-                        спектр инструментов для
-                        глубокого и гибкого
-                        управления продажами,
-                        возможно через несколько
-                        магазинов
-                    </div>
-                </div>
+            </div>
+            <div class="description">
+                <h1 class="description-line">
+                    Иметь весь возможный
+                    спектр инструментов для
+                    глубокого и гибкого
+                    управления продажами,
+                    возможно через несколько
+                    магазинов
+                </h1>
             </div>
         </div>
     </div>
@@ -288,293 +313,82 @@ $tag-width: 1150px;
         }
     }
 
-    .view-tariffs-block {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin: 50px 0;
+    .tariff-description {
+        font-weight: 700;
+        font-size: 48px;
+        line-height: 59px;
+        text-align: center;
         color: #FFFFFF;
-        background-color: #191919;
-        width: 100%;
+        margin: 70px 0px;
+    }
+
+    .tariff-types-wrapper {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        max-width: 1100px;
+        justify-items: flex-start;
+        margin: 50px 0px;
+        column-gap: 30px;
 
         .tariffs-name {
-            padding-bottom: 50px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
 
-            h1 {
+            .base {
+                background: #F0F0F0;
                 font-weight: 700;
-                font-size: 48px;
-                line-height: 59px;
-            }
-        }
-
-        .tariffs-wrapper {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            align-items: center;
-            column-gap: 19px;
-            max-width: 1000px;
-
-            .tariff-side-wrapper {
-                display: flex;
-                flex-direction: row;
-                height: 100%;
-                overflow: hidden;
-                align-items: center;
-                padding-block: 20px;
-            }
-
-            .tariff {
-                display: flex;
-                height: 100%;
-                overflow: hidden;
-                flex-direction: column;
-                align-items: center;
+                font-size: 24px;
+                line-height: 29px;
                 text-align: center;
-                justify-content: space-between;
-                border: 1px solid #B7FF44;
-                border-radius: 24px;
-                padding: 10px;
+                color: #000000;
+                border-radius: 17px;
+                padding: 10px 30px;
+            }
 
-                h1 {
-                    margin: 20px;
-                }
+            .advanced {
+                font-weight: 700;
+                font-size: 24px;
+                line-height: 29px;
+                text-align: center;
+                color: #FFFFFF;
+                background: #7026EB;
+                border-radius: 17px;
+                padding: 10px 30px;
+            }
 
-                .board-info {
-                    display: flex;
-                    flex-direction: column;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-
-                    span {
-                        padding-block: 40px;
-                    }
-                }
-
-                &.second-tariff {
-                    background: #B7FF44;
-                    color: #000000;
-                    border: 1px solid #B7FF44;
-                }
-
-                &.first-tariff,
-                &.third-tariff {
-                    margin-block: 20px;
-                }
-
-                .simple-button-try {
-                    padding: 10px;
-                    background: #B7FF44;
-                    border: 1px black solid;
-                    color: #000000;
-                    border-radius: 12px;
-                    font-weight: 700;
-                    font-size: 23px;
-                    line-height: 28px;
-                    margin: 10px;
-                }
+            .professional {
+                font-weight: 700;
+                font-size: 24px;
+                line-height: 29px;
+                text-align: center;
+                color: #000000;
+                background: #B7FF44;
+                border-radius: 17px;
+                padding: 10px 30px;
             }
         }
-    }
 
-    @media (max-width: $tag-width) {
-        .view-tariffs-block {
-            padding: 0px 40px;
-
-            .tariffs-name {
-                padding: 0px;
-
-                h1 {
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                }
-            }
-
-            .tariffs-wrapper {
-                display: grid;
-                grid-template-columns: 1fr;
-                gap: 20px;
-
-                .tariff-side-wrapper {
-                    overflow: hidden;
-                }
-
-                .tariff {
-                    overflow: hidden;
-
-                    h1 {}
-
-                    .board-info {
-                        line-height: 24px;
-                        font-weight: 700;
-                        font-size: 20px;
-
-                        span {
-                            padding-block: 25px;
-                        }
-                    }
-
-                    &.second-tariff {}
-
-                    &.first-tariff,
-                    &.third-tariff {
-                        margin-block: 20px;
-                    }
-
-                    .simple-button-try {
-                        line-height: 24px;
-                        font-size: 20px;
-                        font-weight: 700;
-
-                    }
-                }
-            }
+        .description-white-line {
+            border-right: 2px solid #FFFFFF;
+            padding: 0px 50px 0px 0px;
         }
-    }
 
-    .tariff-description {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin: 50px;
-
-        .question-tarif {
+        .description {
             font-weight: 700;
-            font-size: 48px;
-            line-height: 59px;
-            color: #FFFFFF;
-        }
+            font-size: 24px;
+            line-height: 29px;
+            color: #D9D9D9;
+            padding-top: 40px;
 
-        .type-description {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            max-width: 1200px;
-            margin: 50px 0px;
-            gap: 40px;
-
-            .base-description {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                padding: 10px;
-                gap: 20px;
-                max-width: 300px;
-                .base-name {
-                    display: flex;
-                    flex-direction: column;
-                    background: #F0F0F0;
-                    border-radius: 17px;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #000000;
-                    text-align: center;
-                    padding: 10px 20px;
-
-                }
-
-                .base-name-description-1 {
-                    border-bottom: 1px solid #B7FF44;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-                }
-
-                .base-name-description-2 {
-
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-                }
-            }
-
-            .advanced-description {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                padding: 10px;
-                gap: 20px;
-                max-width: 300px;
-                .advanced-name {
-                    display: flex;
-                    flex-direction: column;
-                    background: rgba(112, 38, 235, 1);
-                    border-radius: 17px;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: rgba(255, 255, 255, 1);
-                    text-align: center;
-                    padding: 10px 20px;
-                }
-
-                .advanced-name-description-1 {
-                    border-bottom: 1px solid #B7FF44;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-                }
-
-                .advanced-name-description-2 {
-                    padding: 20px 50px 20px 0px;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-                }
-            }
-
-            .professional-description {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                padding: 10px;
-                gap: 20px;
-                max-width: 300px;
-
-                .professional-name {
-                    display: flex;
-                    flex-direction: column;
-                    background: rgba(183, 255, 68, 1);
-                    border-radius: 17px;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: rgba(0, 0, 0, 1);
-                    text-align: center;
-                    padding: 10px 20px;
-                }
-
-                .professional-name-description-1 {
-
-                    border-bottom: 1px solid #B7FF44;
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-
-                }
-
-                .professional-name-description-2 {
-                    font-weight: 700;
-                    font-size: 24px;
-                    line-height: 29px;
-                    color: #D9D9D9;
-
-                }
+            .description-line {
+                border-top: 1px solid #B7FF44;
+                padding: 20px 0px;
             }
         }
+
     }
 
     @media (max-width: $tag-width) {}
-}</style>
+}
+</style>
