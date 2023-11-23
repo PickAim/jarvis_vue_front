245f9759ccfa0e9c06770c5c5dee32055ee567d7
KirillBuiko <k.buiko04@mail.ru>
2023-11-23 23:09:43 +0700

    Create default page preloader

---
+++ src/components/generals/DefaultPagePreloader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import BlinkingPreloader from "@/components/generals/BlinkingPreloader.vue";
+</script>
+
+<template>
+  <div class="unit-economy-preloader-wrapper">
+    <BlinkingPreloader class="label"></BlinkingPreloader>
+    <BlinkingPreloader class="label"></BlinkingPreloader>
+    <div class="products-wrapper">
+      <BlinkingPreloader class="product" :key="i" v-for="i in Array(3).map((i, ind) => ind)"></BlinkingPreloader>
+    </div>
+    <BlinkingPreloader class="select"></BlinkingPreloader>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.unit-economy-preloader-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  .label {
+    height: 25px;
+    width: 300px;
+  }
+
+  .select {
+    height: 40px;
+    width: 250px;
+  }
+
+  .products-wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+
+    .product {
+      width: 150px;
+      aspect-ratio: 1/1;
+    }
+  }
+}
+</style>
