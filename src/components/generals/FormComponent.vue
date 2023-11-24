9bb055edc70e17bb7a61cd1b58abc1bfb293dd82
KirillBuiko <k.buiko04@mail.ru>
2023-11-25 02:46:06 +0700

    Add submit by enter support

---
+++ src/components/generals/FormComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "submit"): void
+}>()
+
+function onKeyDown(e: KeyboardEvent) {
+  if (e.key === "Enter") {
+    emit("submit");
+  }
+}
+</script>
+
+<template>
+  <div class="form-wrapper" @keydown="onKeyDown">
+    <slot/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
